60825daa122c9da34b3ff47f2907b7a549c2678d
Anna Lindqvist <54626932+perhapsimanna@users.noreply.github.com>
2019-09-24 19:42:13 +0200

    net

--- PhotoShopLite/ImageManipulationInConsoleApp/ImageManipulationConsolApp.cs
+++ PhotoShopLite/ImageManipulationInConsoleApp/ImageManipulationConsolApp.cs
@@ -12,24 +12,43 @@ namespace ImageManipulationInConsoleApp
             Console.Write("Input your path to the picture you want to alternate here: ");
             var inputPath = Console.ReadLine();
             var inputImage = Image.FromFile(inputPath);
-            var savePath = Path.GetDirectoryName(inputPath);
-            var saveName = Path.GetFileNameWithoutExtension(inputPath);
-            var saveExtension = Path.GetExtension(inputPath);
-            var inputPicture = new Bitmap(inputImage);
-            var inputPicture1 = new Bitmap(inputImage);
-            var inputPicture2 = new Bitmap(inputImage);
-            var negativePicture = ImageModification.MakeNegativePicture(inputPicture);
-            var blurredPicture = ImageModification.MakeBlurredPicture(inputPicture1);
-            var grayscalePicture = ImageModification.MakeGrayscalePicture(inputPicture2);
-            var combindingGrayscale = Path.Combine(savePath + "\\" + saveName + "_GRAYSCALE" + saveExtension);
-            var combindingBlurred = Path.Combine(savePath + "\\" + saveName + "_BLURRED" + saveExtension);
-            var combindingNegative = Path.Combine(savePath + "\\" + saveName + "_NEGATIVE" + saveExtension);
-            grayscalePicture.Save(combindingGrayscale);
-            blurredPicture.Save(combindingBlurred);
-            negativePicture.Save(combindingNegative);
+            //var inputImageWidth = inputPath.Length;
+            //var inputImageHeight = inputImage.Height;
+            var minSize = 300 * 300;
+            var maxSize = 1920 * 1080;
+            
+            if (inputPath == null)
+            {
+                Console.WriteLine("You need to input a path to a picture! Try again!");
+            }
+            else if (inputImage < minSize)//måste fixa, try catch?
+            {
+                Console.WriteLine("Your input picture is too small! Try again!");
+            }
+            else if (inputPath > maxSize)//måste fixe, try catch?
+            {
+                Console.WriteLine("Your input pictrue is too big! Try again!");
+            }
+            else
+            {
+                var savePath = Path.GetDirectoryName(inputPath);
+                var saveName = Path.GetFileNameWithoutExtension(inputPath);
+                var saveExtension = Path.GetExtension(inputPath);
+                var inputPicture = new Bitmap(inputImage);
+                var inputPicture1 = new Bitmap(inputImage);
+                var inputPicture2 = new Bitmap(inputImage);
+                var negativePicture = ImageModification.MakeNegativePicture(inputPicture);
+                var blurredPicture = ImageModification.MakeBlurredPicture(inputPicture1);
+                var grayscalePicture = ImageModification.MakeGrayscalePicture(inputPicture2);
+                var combindingGrayscale = Path.Combine(savePath + "\\" + saveName + "_GRAYSCALE" + saveExtension);
+                var combindingBlurred = Path.Combine(savePath + "\\" + saveName + "_BLURRED" + saveExtension);
+                var combindingNegative = Path.Combine(savePath + "\\" + saveName + "_NEGATIVE" + saveExtension);
+                grayscalePicture.Save(combindingGrayscale);
+                blurredPicture.Save(combindingBlurred);
+                negativePicture.Save(combindingNegative);
 
-            Console.WriteLine("SUCCSES");
-
+                Console.WriteLine("SUCCSES");
+            }
             
 
 
