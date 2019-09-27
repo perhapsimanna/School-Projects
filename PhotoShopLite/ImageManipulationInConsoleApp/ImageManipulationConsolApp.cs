6ddf787245c6e7481d1083b51add8d5f350510b9
Anna Lindqvist <54626932+perhapsimanna@users.noreply.github.com>
2019-09-27 22:17:57 +0200

    .NET

--- PhotoShopLite/ImageManipulationInConsoleApp/ImageManipulationConsolApp.cs
+++ PhotoShopLite/ImageManipulationInConsoleApp/ImageManipulationConsolApp.cs
@@ -9,42 +9,46 @@ namespace ImageManipulationInConsoleApp
     {
         static void Main(string[] args)
         {
-            //C:\Users\90annlin\Downloads\Gramse.jpg
-            Console.Write("Input your path to a picture you want to alternate here: ");
+            Console.Write("Write the path to the picture you want to alternate here: ");
+
             var inputPath = Console.ReadLine();
             var inputImage = Image.FromFile(inputPath);
             var inputImageWidth = inputImage.Width;
             var inputImageHeight = inputImage.Height;
-                       
-            if (inputPath == null || inputImage == null)//try catch
+
+            if (inputImageWidth < 200 && inputImageHeight < 200)
             {
-                Console.WriteLine("You need to input a path to a picture! Try again!");
-            }
-            else if (inputImageWidth < 100 && inputImageHeight < 100)
-            {
-                Console.WriteLine("Your input picture is too small! Try again!");
+                Console.WriteLine("Your input picture cannot be smaller then 200 x 200! Try again!");
             }
             else if (inputImageWidth > 1920 && inputImageHeight > 1080)
             {
-                Console.WriteLine("Your input pictrue is too big! Try again!");
+                Console.WriteLine("Your input pictrue cannot be bigger then 1920 x 1080! Try again!");
             }
             else
             {
-                
-                
                 var inputPicture = new Bitmap(inputImage);
                 var inputPictureOne = new Bitmap(inputImage);
                 var inputPictureTwo = new Bitmap(inputImage);
                 ImageModification.MakeNegativePicture(inputPicture, inputPath);
                 ImageModification.MakeBlurredPicture(inputPictureOne, inputPath);
                 ImageModification.MakeGrayscalePicture(inputPictureTwo, inputPath);
-                
-
-                Console.WriteLine("SUCCSES");
             }
-            
+
+            /*Löser inte detta
+            try
+            {
 
 
+            }
+            catch (FileNotFoundException fileNotFound)
+            {
+                Console.WriteLine("The file was not found, please try again", fileNotFound);
+            }
+            catch (ArgumentException noFileInput)
+            {
+                Console.WriteLine("You need to enter a path to an imagefile that you would like to modify", noFileInput);
+            }
+            */
         }
     }
 }
